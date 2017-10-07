4c527302a3dd0d9ecff6206571c89dd8236e85d6
Alexandru-Paul Copil (thee-engineer) <alexandru.p.copil@gmail.com>
2017-10-07 10:40:16 +0100

    Implemented custom ECDSA in cryptor/crypt

---
+++ crypt/ecdsa.go
@@ -0,0 +1,127 @@
+package crypt
+
+import (
+	"crypto"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"errors"
+	"hash"
+	"math/big"
+
+	"golang.org/x/crypto/sha3"
+)
+
+const (
+	secLen = 33
+	macLen = 33
+)
+
+// PublicKey is a substitute for ecdsa.PublicKey.
+type PublicKey struct {
+	elliptic.Curve
+	X, Y   *big.Int
+	Params *Param
+}
+
+// ImportPublic a Go ECDSA Public key as a custom Public key.
+func ImportPublic(pub *ecdsa.PublicKey) *PublicKey {
+	return &PublicKey{
+		Curve:  pub.Curve,
+		X:      pub.X,
+		Y:      pub.Y,
+		Params: ECDSA521Param,
+	}
+}
+
+// Export converts a custom ECDSA Public key to Go standard ECDSA Public key.
+func (pub *PublicKey) Export() *ecdsa.PublicKey {
+	return &ecdsa.PublicKey{Curve: pub.Curve, X: pub.X, Y: pub.Y}
+}
+
+// Param is information about the ECDSA Curve
+type Param struct {
+	Hash      func() hash.Hash
+	hType     crypto.Hash
+	Cipher    func([]byte) (cipher.Block, error)
+	BlockSize int
+	KeyLength int
+}
+
+var (
+	// ECDSA521Param are all the standard parameters for Curve 521
+	ECDSA521Param = &Param{
+		Hash:      sha3.New512,   // Using 512 sha3 function
+		hType:     crypto.SHA512, // Using 512 sha3
+		Cipher:    aes.NewCipher, // Asymetric cipher block
+		BlockSize: aes.BlockSize, // Asymetric cipher block size
+		KeyLength: 32}            // Length of the asymetric key
+)
+
+// PrivateKey is a substitute for ecdsa.PrivateKey.
+type PrivateKey struct {
+	PublicKey
+	D *big.Int
+}
+
+// Import a Go ECDSA Private key as custom Private key.
+func Import(prv *ecdsa.PrivateKey) *PrivateKey {
+	pub := ImportPublic(&prv.PublicKey)
+	return &PrivateKey{*pub, prv.D}
+}
+
+// Export converts a custom ECDSA Private key to Go standard ECDSA Private key.
+func (prv *PrivateKey) Export() *ecdsa.PrivateKey {
+	customPublicKey := &prv.PublicKey
+	ecdsaPublicKey := customPublicKey.Export()
+	return &ecdsa.PrivateKey{PublicKey: *ecdsaPublicKey, D: prv.D}
+}
+
+// GenerateKey generates a new Private/Public key pair using P521 Curve
+// which implements P-521 (see FIPS 186-3, section D.2.5).
+func GenerateKey() (*PrivateKey, error) {
+	pb, x, y, err := elliptic.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	prv := new(PrivateKey)
+	prv.PublicKey.X = x
+	prv.PublicKey.Y = y
+	prv.PublicKey.Curve = elliptic.P521()
+	prv.D = new(big.Int).SetBytes(pb)
+
+	return prv, nil
+}
+
+// GenerateShared creates a new shared secret of given lenght.
+func (prv *PrivateKey) GenerateShared(pub *PublicKey) (sec []byte, err error) {
+	// Verify matching curves on both public and private keys
+	if prv.PublicKey.Curve != pub.Curve {
+		return nil, errors.New("shared key: invalid curve match")
+	}
+
+	// Verify shared secret length
+	if secLen+macLen > pub.SecretLength() {
+		return nil, errors.New("shared key: length too big")
+	}
+
+	// Verify new point position
+	np, _ := pub.Curve.ScalarMult(pub.X, pub.Y, prv.D.Bytes())
+	if np == nil {
+		return nil, errors.New("shared key: invalid point position")
+	}
+
+	// Generate shared secret
+	sec = make([]byte, secLen+macLen)
+	secBytes := np.Bytes()
+	copy(sec[len(sec)-len(secBytes):], secBytes)
+	return sec, nil
+}
+
+// SecretLength returns the maximum length of the shared secret.
+func (pub *PublicKey) SecretLength() int {
+	return (pub.Curve.Params().BitSize + 7) / 8
+}
---
+++ crypt/ecdsa_test.go
@@ -0,0 +1,74 @@
+package crypt
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestImportExport(t *testing.T) {
+	t.Parallel()
+
+	// Generate key pair
+	prv, err := GenerateKey()
+	if err != nil {
+		t.Error(err)
+	}
+	pub := prv.PublicKey
+
+	// Export to Go ECDSA Keys
+	ecdsaPrv := prv.Export()
+	ecdsaPub := pub.Export()
+
+	// Import private key
+	impPrv := Import(ecdsaPrv)
+
+	if !bytes.Equal(impPrv.D.Bytes(), prv.D.Bytes()) {
+		t.Error("ecdsa: imported and inital key mismatch, private")
+	}
+
+	// Import public key
+	impPub := ImportPublic(ecdsaPub)
+
+	if !bytes.Equal(impPub.X.Bytes(), pub.X.Bytes()) {
+		t.Error("ecdsa: imported and inital key mismatch, public")
+	}
+
+	if !bytes.Equal(impPub.Y.Bytes(), pub.Y.Bytes()) {
+		t.Error("ecdsa: imported and inital key mismatch, public")
+	}
+}
+
+func TestSharedSecret(t *testing.T) {
+	t.Parallel()
+
+	// Generate first key pair
+	prv0, err := GenerateKey()
+	if err != nil {
+		t.Error(err)
+	}
+	pub0 := &prv0.PublicKey
+
+	// Generate second key pair
+	prv1, err := GenerateKey()
+	if err != nil {
+		t.Error(err)
+	}
+	pub1 := &prv1.PublicKey
+
+	// Obtain shared secret from first private key and second public key
+	ss0, err := prv0.GenerateShared(pub1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Obtain shared secret from second private key and first public key
+	ss1, err := prv1.GenerateShared(pub0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Compare the two secrets
+	if !bytes.Equal(ss0, ss1) {
+		t.Errorf("shared secret: not matching")
+	}
+}
