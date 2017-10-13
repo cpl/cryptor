060aca903d46643464ca06d33a3bf9f59760b3af
Alexandru-Paul Copil (thee-engineer) <alexandru.p.copil@gmail.com>
2017-10-13 17:02:49 +0100

    Move peer operations to p2p/peer

--- p2p/peer.go
+++ p2p/peer.go
@@ -18,3 +18,39 @@ func NewPeer(ip string, tcp, udp int) *Peer {
 		tcpAddr: &net.TCPAddr{Port: tcp, IP: net.ParseIP(ip)},
 	}
 }
+
+// AddPeer adds a given peer to the peer memory map.
+func (n *Node) AddPeer(peer *Peer) {
+	select {
+	case n.addp <- peer:
+	}
+}
+
+// Peers returns a list of all peers related to this Node.
+func (n *Node) Peers() []*Peer {
+	var peerList []*Peer
+
+	select {
+	case n.pops <- func(peers map[string]*Peer) {
+		for _, p := range peers {
+			peerList = append(peerList, p)
+		}
+	}:
+		<-n.popd
+	}
+
+	return peerList
+}
+
+// PeerCount returns the number of related peers for this Node.
+func (n *Node) PeerCount() int {
+	var count int
+
+	select {
+	case n.pops <- func(peerList map[string]*Peer) { count = len(peerList) }:
+		<-n.popd
+	case <-n.quit:
+	}
+
+	return count
+}
