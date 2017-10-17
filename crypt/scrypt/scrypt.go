ed0d3f9d547ed6934bc3eb27cb1796bdc9b120a7
Alexandru-Paul Copil (thee-engineer) <alexandru.p.copil@gmail.com>
2017-10-17 21:15:59 +0100

    Add crypt/scrypt

---
+++ crypt/scrypt/scrypt.go
@@ -0,0 +1,40 @@
+package scrypt
+
+import (
+	"github.com/thee-engineer/cryptor/crypt"
+	"golang.org/x/crypto/scrypt"
+)
+
+const keySize = 32
+
+// Scrypt takes a password and salt and derives a 32 byte key.
+func Scrypt(password string, salt []byte) []byte {
+	// Derive 32 byte key.
+	key, err := scrypt.Key([]byte(password), salt, 1048576, 8, 1, keySize)
+	if err != nil {
+		panic(err)
+	}
+
+	return key
+}
+
+// RandomSalt applies the scrypt derivation function using a given password
+// and a random 16 byte salt.
+func RandomSalt(password string) ([]byte, []byte) {
+	// Generate random salt.
+	salt := crypt.RandomData(16)
+
+	return Scrypt(password, salt), salt
+}
+
+// AllRandom generates both a random password and salt, then derives a new
+// 32byte key using scrypt.
+func AllRandom() ([]byte, []byte, []byte) {
+	// Generate random salt.
+	salt := crypt.RandomData(16)
+
+	// Generate random password.
+	pass := crypt.RandomData(64)
+
+	return Scrypt(string(pass), salt), pass, salt
+}
---
+++ crypt/scrypt/scrypt_test.go
@@ -0,0 +1,41 @@
+package scrypt_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/thee-engineer/cryptor/crypt/scrypt"
+)
+
+func TestScrypt(t *testing.T) {
+	t.Parallel()
+
+	key0 := scrypt.Scrypt("hello world", []byte("easy salt"))
+	key1 := scrypt.Scrypt("hello world", []byte("easy salt"))
+
+	if !bytes.Equal(key0, key1) {
+		t.Error("scrypt: derived keys don't match")
+	}
+}
+
+func TestScryptRandomSalt(t *testing.T) {
+	t.Parallel()
+
+	key0, salt := scrypt.RandomSalt("hello world")
+	key1 := scrypt.Scrypt("hello world", salt)
+
+	if !bytes.Equal(key0, key1) {
+		t.Error("scrypt: derived keys don't match")
+	}
+}
+
+func TestScryptAllRandom(t *testing.T) {
+	t.Parallel()
+
+	key0, pass, salt := scrypt.AllRandom()
+	key1 := scrypt.Scrypt(string(pass), salt)
+
+	if !bytes.Equal(key0, key1) {
+		t.Error("scrypt: derived keys don't match")
+	}
+}
