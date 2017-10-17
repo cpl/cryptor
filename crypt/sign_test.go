4e070bb30b07dee52cf7b57fa2c6c225938e0a2b
Alexandru-Paul Copil (thee-engineer) <alexandru.p.copil@gmail.com>
2017-10-17 15:03:03 +0100

    Add tests for sign/verify on shared keys

--- crypt/sign_test.go
+++ crypt/sign_test.go
@@ -1,6 +1,7 @@
 package crypt_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/thee-engineer/cryptor/crypt"
@@ -87,22 +88,77 @@ func TestSignInvalid(t *testing.T) {
 	}
 }
 
-func TestSignSharedSecret(t *testing.T) {
+func TestSignVerifySharedSecret(t *testing.T) {
 	t.Parallel()
 
-	key0, err := crypt.GenerateKey256()
+	prv0, prv1, err := generateKeyPair256()
 	if err != nil {
 		t.Error(err)
 	}
 
-	key1, err := crypt.GenerateKey256()
+	sec0, err := prv0.GenerateShared(&prv1.PublicKey, 16, 16)
 	if err != nil {
 		t.Error(err)
 	}
 
-	// TODO: Finish test
-	_, err = key0.GenerateShared(&key1.PublicKey, 16, 16)
+	sec1, err := prv1.GenerateShared(&prv0.PublicKey, 16, 16)
 	if err != nil {
 		t.Error(err)
+	}
+
+	if !bytes.Equal(sec0, sec1) {
+		t.Error("shared keys: mismatched secrets")
+	}
+
+	sec1Key, err := crypt.Key256FromSecret(sec1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	sec0Key, err := crypt.Key256FromSecret(sec0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !compareKeys(sec0Key, sec1Key) {
+		t.Error("shared keys: not matching")
+	}
+
+	testData := crypt.RandomData(1024)
+
+	sign0, err := sec0Key.Sign(testData)
+	if err != nil {
+		t.Error(err)
+	}
+
+	sign1, err := sec1Key.Sign(testData)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !sec0Key.Verify(testData, sign0) {
+		t.Error("sign: failed to verify test data with signature")
+	}
+
+	if !sec0Key.Verify(testData, sign1) {
+		t.Error("sign: failed to verify test data with signature")
+	}
+
+	if !sec1Key.Verify(testData, sign0) {
+		t.Error("sign: failed to verify test data with signature")
+	}
+
+	if !sec1Key.Verify(testData, sign1) {
+		t.Error("sign: failed to verify test data with signature")
+	}
+
+	// Invalid signature
+	if sec0Key.Verify(testData, crypt.RandomData(32)) {
+		t.Error("sign: verified invalid signature")
+	}
+
+	// Invalid data
+	if sec0Key.Verify(crypt.RandomData(1024), sign0) {
+		t.Error("sign: verified invalid data")
 	}
 }
