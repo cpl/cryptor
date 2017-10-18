bf59ede9e80dd5f527367d7e97d9b470f5009b33
Alexandru-Paul Copil (thee-engineer) <alexandru.p.copil@gmail.com>
2017-10-18 23:22:37 +0100

    Progress with Go HTML Temples

--- cmd/cryptor-web/cryptor-web.go
+++ cmd/cryptor-web/cryptor-web.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"html/template"
+	"net/http"
+)
 
+type Page struct {
+	Content template.Template
+}
+
+func main() {
+	templates := template.Must(template.ParseGlob("templates/*.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		templates.ExecuteTemplate(w, "default.html", "")
+	})
+
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("templates/css"))))
+
+	http.ListenAndServe(":9000", nil)
 }
