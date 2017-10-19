cf98162ae839e65187b6d098ba8af46bf2535972
Alexandru-Paul Copil (thee-engineer) <alexandru.p.copil@gmail.com>
2017-10-19 12:06:22 +0100

    Improving web interface (AVOID USEING IT IN PRODUCTION FOR NOW)

--- cmd/cryptor-web/cryptor-web.go
+++ cmd/cryptor-web/cryptor-web.go
@@ -5,14 +5,52 @@ import (
 	"net/http"
 )
 
+// Parse templates
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+
 func main() {
-	templates := template.Must(template.ParseGlob("templates/*.html"))
+
+	page := &Page{
+		Sidebar: Sidebar{
+			IP:   "192.168.0.103",
+			Port: 9000,
+		},
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		templates.ExecuteTemplate(w, "base.html", "")
+		templates.ExecuteTemplate(w, "base.html", page)
 	})
 
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("templates/css"))))
+	// Handle assets
+	http.Handle("/css/", http.StripPrefix(
+		"/css/", http.FileServer(http.Dir("templates/css"))))
 
 	http.ListenAndServe(":9000", nil)
+}
+
+func exection(t template.Template) template.HTML {
+	b := bytes.Buffer{}
+    t.ExecuteTemplate(&b, params.Name, nil)
+	return template.HTML(b.String())
+}
+
+// Cache ...
+type Cache struct {
+	Path       string
+	Size       int
+	MaxSize    int
+	Percent    float32
+	ChunkCount int
+}
+
+// Sidebar ...
+type Sidebar struct {
+	IP   string
+	Port int
+}
+
+// Page ...
+type Page struct {
+	Sidebar Sidebar
+	Caches  []Cache
 }
