513ba0e42bb1a46635eef0d8bee6b5758ecc396f
Alexandru-Paul Copil (thee-engineer) <alexandru.p.copil@gmail.com>
2017-11-14 15:06:26 +0000

    Switching CLI from mkideal to urfave

--- cmd/cryptor-cli/cryptor-cli.go
+++ cmd/cryptor-cli/cryptor-cli.go
@@ -1,67 +1,66 @@
 // cryptor
-//
-//    Usage: cryptor chunk
-//
-// -h, --help             display help information
-// --source               *provide source file/dir for chunker
-// --size[=1048576]       chunk size, default 1MB
-// --list                 lists all chunks in the cache
-// -p, --password         password for encryption
-// -c, --cache[=default]  *provide cache source
-//
-//    Usage: cryptor assemble
-//
-// -h, --help             display help information
-// --hash                 *tail hash, point of start for assembly
-// -p, --password         password for encryption
-// -c, --cache[=default]  *provide cache source
 package main
 
 import (
-	"fmt"
 	"os"
+	"time"
 
-	"github.com/mkideal/cli"
+	"github.com/urfave/cli"
 )
 
-var help = cli.HelpCommand("display this help message")
+const cryptorName = "cryptor"
+const cryptorVersion = "0.0.3"
+const cryptorCopyright = "Copyright (C) 2017 Alexandru-Paul Copil"
+const cryptorUsageDescription = "command line interface for basic tasks"
+const cryptorUsageString = " [command] [sub-command] [args]"
 
 func main() {
-	// Tree with all possible subcommands
-	if err := cli.Root(root,
-		cli.Tree(help),
-		cli.Tree(chunkCLI),
-		cli.Tree(assembleCLI),
-		cli.Tree(cacheCLI,
-			cli.Tree(newCacheCLI),
-			cli.Tree(listCacheCLI),
-		),
-	).Run(os.Args[1:]); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	app := cli.NewApp()
+
+	app.EnableBashCompletion = true
+	app.Version = cryptorVersion
+	app.Name = cryptorName
+	app.Compiled = time.Now()
+	app.Usage = cryptorUsageDescription
+	app.UsageText = cryptorName + cryptorUsageString
+	app.Copyright = cryptorCopyright
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "chunk",
+			Usage: "chunk a file given a input package and output cache",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name:  "assemble",
+			Usage: "assemble a package given the key/pass and input cache",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name:  "cache",
+			Usage: "diffrent commands for interacting with caches",
+			Subcommands: []cli.Command{
+				{
+					Name:  "add",
+					Usage: "add a new cache",
+					Action: func(c *cli.Context) error {
+						return nil
+					},
+				},
+				{
+					Name:  "remove",
+					Usage: "remove an existing cache",
+					Action: func(c *cli.Context) error {
+						return nil
+					},
+				},
+			},
+		},
 	}
-}
 
-// Print error and exit program
-func handleErr(err error) {
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-}
-
-// Arguments that cen be used in root
-type rootArg struct {
-	cli.Helper
-}
-
-// The way root treats arguments
-var root = &cli.Command{
-	Desc: "cryptor, file sharing redefined",
-	Argv: func() interface{} { return new(rootArg) },
-	Fn: func(ctx *cli.Context) error {
-		// argv := ctx.Argv().(*rootArg)
-		// ctx.String("%s\n", argv)
-		return nil
-	},
+	app.Run(os.Args)
 }
