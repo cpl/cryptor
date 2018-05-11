87ae92496b2957df0be86f2c3f61c556c4fc67e7
Alexandru-Paul Copil (copper) <alexandru.p.copil@gmail.com>
2018-05-11 22:34:09 +0100

    Rename encoding input to data

--- crypt/encoding.go
+++ crypt/encoding.go
@@ -4,29 +4,29 @@ import (
 	"encoding/hex"
 )
 
-// Encode returns the given msg as hex encoded []byte
-func Encode(msg []byte) []byte {
-	out := make([]byte, hex.EncodedLen(len(msg)))
-	hex.Encode(out, msg)
+// Encode returns the given data as hex encoded []byte
+func Encode(data []byte) []byte {
+	out := make([]byte, hex.EncodedLen(len(data)))
+	hex.Encode(out, data)
 	return out
 }
 
-// Decode takes a hex encoded msg and returns the decoded msg
-func Decode(msg []byte) ([]byte, error) {
-	out := make([]byte, hex.DecodedLen(len(msg)))
-	read, err := hex.Decode(out, msg)
+// Decode takes a hex encoded data and returns the decoded data
+func Decode(data []byte) ([]byte, error) {
+	out := make([]byte, hex.DecodedLen(len(data)))
+	read, err := hex.Decode(out, data)
 	if err != nil {
 		return nil, err
 	}
 	return out[:read], nil
 }
 
-// EncodeString returns the given msg as hex encoded string
-func EncodeString(msg []byte) string {
-	return string(Encode(msg))
+// EncodeString returns the given data as hex encoded string
+func EncodeString(data []byte) string {
+	return string(Encode(data))
 }
 
 // DecodeString takes a hex encoded string and returns the decoded []byte
-func DecodeString(msg string) ([]byte, error) {
-	return Decode([]byte(msg))
+func DecodeString(data string) ([]byte, error) {
+	return Decode([]byte(data))
 }
