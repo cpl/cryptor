77b9941d1efe874fd4f9a9bcb11b3164b3fa5a2d
Alexandru-Paul Copil (copper) <alexandru.p.copil@gmail.com>
2018-05-12 14:29:32 +0100

    Add new constants for bytes

--- cachedb/defaults.go
+++ cachedb/defaults.go
@@ -1,11 +1,13 @@
 package cachedb
 
+import "github.com/thee-engineer/cryptor/common/con"
+
 // DefaultManagerConfig is the default recommended configuration for
 // caches in GENERAL!
 var DefaultManagerConfig = ManagerConfig{
-	MaxCacheSize:      268435456,
-	MaxChunkCount:     2048,
-	MaxChunkSize:      1048576,
+	MaxCacheSize:      1 * con.GB,
+	MaxChunkCount:     1000,
+	MaxChunkSize:      10 * con.MB,
 	MinChunkSize:      0,
 	CurrentCacheSize:  0,
 	CurrentChunkCount: 0,
---
+++ common/con/bytes.go
@@ -0,0 +1,7 @@
+package con
+
+const (
+	KB = 1024
+	MB = 1048576
+	GB = 1073742000
+)
