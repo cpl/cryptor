a7a4e1d11e99ac57c32b287f0b73c49fad398e47
Alexandru-Paul Copil (thee-engineer) <alexandru.p.copil@gmail.com>
2018-05-17 18:01:34 +0100

    Add encryption support to config files

--- config/config_test.go
+++ config/config_test.go
@@ -1,4 +1,4 @@
-package config_test
+package config
 
 import (
 	"os"
@@ -6,15 +6,44 @@ import (
 	"testing"
 
 	"github.com/spf13/viper"
-
-	_ "github.com/thee-engineer/cryptor/config"
+	"github.com/thee-engineer/cryptor/crypt/aes"
 )
 
 var configFile = path.Join(
-	os.Getenv("GOPATH"), "src/github.com/thee-engineer/cryptor/config.json")
+	os.Getenv("GOPATH"),
+	"src/github.com/thee-engineer/cryptor/config_test.json")
 
+func TestConfigNotFound(t *testing.T) {
+	viper.Reset()
+
+	if err := readConfig("no-such-config"); err != nil {
+		if err.Error() != "open : no such file or directory" {
+			t.Fatal(err)
+		}
+		return
+	}
+
+	t.Fatalf("found invalid config")
+}
 func TestBasicConfig(t *testing.T) {
-	t.Parallel()
+	viper.Reset()
+	readConfig("config_test")
+	viper.Reset()
+
+	if usedFile := viper.ConfigFileUsed(); usedFile != "" {
+		t.Fatalf("read config file should be none")
+	}
+
+	if len(viper.AllKeys()) != 0 {
+		t.Fatalf("config, not empty")
+	}
+
+	if err := aes.EncryptFiles("testpassword", configFile); err != nil {
+		t.Fatal(err)
+	}
+	if err := readConfig("config_test"); err != nil {
+		t.Fatal(err)
+	}
 
 	if usedFile := viper.ConfigFileUsed(); usedFile != configFile {
 		t.Fatalf("expected config file %s, got %s", configFile, usedFile)
@@ -22,5 +51,9 @@ func TestBasicConfig(t *testing.T) {
 
 	if len(viper.AllKeys()) == 0 {
 		t.Fatalf("read config, empty")
+	}
+
+	if !viper.IsSet("version") {
+		t.Fatalf("failed to find field version")
 	}
 }
