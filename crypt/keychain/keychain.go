101ce262a0db8f845cd144647d393cf471e5c94d
Alexandru-Paul Copil <alexandru@cpl.li>
2019-04-06 21:43:19 +0300

    Remove keychain package, unused
    Keychain will be added later as a management tool

--- crypt/keychain/keychain.go
+++
@@ -1,5 +0,0 @@
-/*
-Package keychain offers functions and structures for managing multiple key pairs
-and their associated resources.
-*/
-package keychain // import "cpl.li/go/cryptor/crypt/keychain"
