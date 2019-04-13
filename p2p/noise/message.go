4f67d4981fa82de78fd1b7521f192aab92bc6cbc
Alexandru-Paul Copil <alexandru@cpl.li>
2019-04-13 22:40:36 +0300

    Add handshake message Marshalling

--- p2p/noise/message.go
+++ p2p/noise/message.go
@@ -1,6 +1,8 @@
 package noise
 
 import (
+	"errors"
+
 	"cpl.li/go/cryptor/crypt/ppk"
 )
 
@@ -11,6 +13,26 @@ type MessageInitializer struct {
 	EncryptedInitializerStaticPublicKey [sizeEncPub]byte
 }
 
+// MarshalBinary encodes the receiver into a binary form and returns the result.
+func (msg *MessageInitializer) MarshalBinary() ([]byte, error) {
+	return append(
+		msg.PlaintextUniquePublic[:],
+		msg.EncryptedInitializerStaticPublicKey[:]...), nil
+}
+
+// UnmarshalBinary can unmarshal the output of MarshalBinary back into itself.
+func (msg *MessageInitializer) UnmarshalBinary(data []byte) error {
+	// check data size
+	if len(data) != HandshakeSizeInitializer {
+		return errors.New("invalid message size")
+	}
+
+	copy(msg.PlaintextUniquePublic[:], data[:ppk.KeySize])
+	copy(msg.EncryptedInitializerStaticPublicKey[:], data[ppk.KeySize:])
+
+	return nil
+}
+
 // MessageResponder encapsulates the data which is sent by the responder to
 // the initializer.
 type MessageResponder struct {
@@ -18,9 +40,22 @@ type MessageResponder struct {
 	EncryptedNothing      [sizeEncNth]byte
 }
 
-// Message is an abstraction for MessageInitializer and MessageResponder.
-type Message interface {
-	Bytes() []byte
-	PublicKey() []byte
-	Payload() []byte
+// MarshalBinary encodes the receiver into a binary form and returns the result.
+func (msg *MessageResponder) MarshalBinary() ([]byte, error) {
+	return append(
+		msg.PlaintextUniquePublic[:],
+		msg.EncryptedNothing[:]...), nil
+}
+
+// UnmarshalBinary can unmarshal the output of MarshalBinary back into itself.
+func (msg *MessageResponder) UnmarshalBinary(data []byte) error {
+	// check data size
+	if len(data) != HandshakeSizeResponder {
+		return errors.New("invalid message size")
+	}
+
+	copy(msg.PlaintextUniquePublic[:], data[:ppk.KeySize])
+	copy(msg.EncryptedNothing[:], data[ppk.KeySize:])
+
+	return nil
 }
---
+++ p2p/noise/message_test.go
@@ -0,0 +1,94 @@
+package noise_test
+
+import (
+	"bytes"
+	"testing"
+
+	"cpl.li/go/cryptor/crypt"
+	"cpl.li/go/cryptor/crypt/ppk"
+	"cpl.li/go/cryptor/p2p/noise"
+)
+
+func TestMarshalMessagesInitializer(t *testing.T) {
+	// generate test message
+	secret, _ := ppk.NewPrivateKey()
+	public := secret.PublicKey()
+	msgI := new(noise.MessageInitializer)
+	msgI.PlaintextUniquePublic = public
+
+	// marshal into binary
+	dataI, err := msgI.MarshalBinary()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// check size
+	if len(dataI) != noise.HandshakeSizeInitializer {
+		t.Fatal("invalid binary form size")
+	}
+
+	// compare key
+	if !bytes.Equal(dataI[:ppk.KeySize], public[:]) {
+		t.Fatal("failed to find key in binary form")
+	}
+
+	newMsgI := new(noise.MessageInitializer)
+
+	// unmarshal with invalid data
+	if err := newMsgI.UnmarshalBinary(crypt.RandomBytes(50)); err == nil {
+		t.Fatal("unmarshal invalid data")
+	}
+
+	// unmarshal
+	if err := newMsgI.UnmarshalBinary(dataI); err != nil {
+		t.Fatal(err)
+	}
+
+	// compare initial message with new message
+	if !bytes.Equal(
+		newMsgI.PlaintextUniquePublic[:], msgI.PlaintextUniquePublic[:]) {
+		t.Fatal("failed to match keys")
+	}
+}
+
+func TestMarshalMessagesResponder(t *testing.T) {
+	// generate test message
+	secret, _ := ppk.NewPrivateKey()
+	public := secret.PublicKey()
+	msgR := new(noise.MessageResponder)
+	msgR.PlaintextUniquePublic = public
+
+	// marshal into binary
+	dataI, err := msgR.MarshalBinary()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// check size
+	if len(dataI) != noise.HandshakeSizeResponder {
+		t.Fatal("invalid binary form size")
+	}
+
+	// compare key
+	if !bytes.Equal(dataI[:ppk.KeySize], public[:]) {
+		t.Fatal("failed to find key in binary form")
+	}
+
+	newMsgR := new(noise.MessageResponder)
+
+	// unmarshal with invalid data
+	if err := newMsgR.UnmarshalBinary(crypt.RandomBytes(50)); err == nil {
+		t.Fatal("unmarshal invalid data")
+	}
+
+	// unmarshal
+	if err := newMsgR.UnmarshalBinary(dataI); err != nil {
+		t.Fatal(err)
+	}
+
+	// compare initial message with new message
+	if !bytes.Equal(
+		newMsgR.PlaintextUniquePublic[:], msgR.PlaintextUniquePublic[:]) {
+		t.Fatal("failed to match keys")
+	}
+}
