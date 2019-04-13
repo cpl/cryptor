99b44c5250bfd25c221beebac9f0e4c233091ada
Alexandru-Paul Copil <alexandru@cpl.li>
2019-04-13 22:40:57 +0300

    Modify handshake protocol to work with messages

--- p2p/noise/handshake_test.go
+++ p2p/noise/handshake_test.go
@@ -18,11 +18,11 @@ func TestHandshake(t *testing.T) {
 	rSPublic := rSSecret.PublicKey()
 
 	// initializer begin protocol
-	iHandshake, encISPub := noise.Initialize(iSPublic, rSPublic)
+	iHandshake, msgI := noise.Initialize(iSPublic, rSPublic)
 
 	// responder receives handshake data
-	rHandshake, iSPublicR, encNothing, err :=
-		noise.Respond(iHandshake.PublicKey(), rSSecret, encISPub)
+	rHandshake, iSPublicR, msgR, err :=
+		noise.Respond(msgI, rSSecret)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -31,8 +31,7 @@ func TestHandshake(t *testing.T) {
 	}
 
 	// responder sends response to initializer
-	if err := iHandshake.Receive(
-		rHandshake.PublicKey(), iSSecret, encNothing); err != nil {
+	if err := iHandshake.Receive(msgR, iSSecret); err != nil {
 		t.Fatal(err)
 	}
 
@@ -60,55 +59,3 @@ func TestHandshake(t *testing.T) {
 
 // TODO Write test cases for invalid states
 // TODO Write test cases for MITM and other attacks
-
-func TestHandshakeWithPackets(t *testing.T) {
-	// initializer
-	iSSecret, _ := ppk.NewPrivateKey()
-	iSPublic := iSSecret.PublicKey()
-
-	// responder
-	rSSecret, _ := ppk.NewPrivateKey()
-	rSPublic := rSSecret.PublicKey()
-
-	// initializer begin protocol
-	iHandshake, encISPub := noise.Initialize(iSPublic, rSPublic)
-
-	// create packet
-
-	// responder receives handshake data
-	rHandshake, iSPublicR, encNothing, err :=
-		noise.Respond(iHandshake.PublicKey(), rSSecret, encISPub)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(iSPublicR[:], iSPublic[:]) {
-		t.Fatalf("failed to match initializer public key")
-	}
-
-	// responder sends response to initializer
-	if err := iHandshake.Receive(
-		rHandshake.PublicKey(), iSSecret, encNothing); err != nil {
-		t.Fatal(err)
-	}
-
-	// both handshakes can compute transport keys
-	iSend, iRecv, err := iHandshake.Finalize()
-	if err != nil {
-		t.Fatal(err)
-	}
-	rSend, rRecv, err := rHandshake.Finalize()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// check for zero key
-	var zeroKey [ppk.KeySize]byte
-	if bytes.Equal(zeroKey[:], rSend[:]) || bytes.Equal(zeroKey[:], iSend[:]) {
-		t.Fatal("transport keys may be zero")
-	}
-
-	// compare keys
-	if !bytes.Equal(rSend[:], iRecv[:]) || !bytes.Equal(iSend[:], rRecv[:]) {
-		t.Fatal("failed to match transport keys")
-	}
-}
