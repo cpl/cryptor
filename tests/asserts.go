e10060fe0d44beeaa06539586fd30a7d920719d9
Alexandru-Paul Copil <alexandru@cpl.li>
2019-04-16 18:33:39 +0300

    Polish tests package

--- tests/asserts.go
+++ tests/asserts.go
@@ -2,17 +2,17 @@ package tests
 
 import "testing"
 
-// AssertNil checks that the err is nil. If the error is something other
-// than nil, then the test will fail and exit with the error message.
-func AssertNil(t *testing.T, err error) {
-	if err != nil {
-		t.Fatal(err)
+// AssertNil checks that the err is nil. If the "object" is something other
+// than nil, then the test will fail and exit with the object.
+func AssertNil(t *testing.T, obj interface{}) {
+	if obj != nil {
+		t.Fatal(obj)
 	}
 }
 
-// AssertEqual makes sure that the two given "interfaces" are NOT diffrent.
-func AssertEqual(t *testing.T, i0 interface{}, i1 interface{}) {
-	if i0 != i1 {
-		t.Fatalf("not equal, expected %v = %v\n", i0, i1)
+// AssertEqual makes sure that the two given "objects" are NOT diffrent (!=).
+func AssertEqual(t *testing.T, obj0 interface{}, obj1 interface{}) {
+	if obj0 != obj1 {
+		t.Fatalf("not equal, expected %v = %v\n", obj0, obj1)
 	}
 }
