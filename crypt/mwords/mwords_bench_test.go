70df5863a6f1b6ba845bee58edf5f9a73f14b05f
Alexandru-Paul Copil <alexandru@cpl.li>
2019-04-26 00:52:15 +0300

    crypt/mwords: add benchmarks

--- crypt/mwords/mwords_bench_test.go
+++ crypt/mwords/mwords_bench_test.go
@@ -1,3 +1,25 @@
 package mwords_test
 
-// TODO Write benchmarks + comparison with github.com/tyler-smith/go-bip39
+import (
+	"testing"
+
+	"cpl.li/go/cryptor/crypt"
+	"cpl.li/go/cryptor/crypt/mwords"
+)
+
+func BenchmarkEntropyToMnemonic(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		mwords.EntropyToMnemonic(crypt.RandomBytes(32))
+	}
+}
+
+func BenchmarkEntropyFromMnemonic(b *testing.B) {
+	mnemonic, err := mwords.EntropyToMnemonic(crypt.RandomBytes(32))
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mwords.EntropyFromMnemonic(mnemonic)
+	}
+}
