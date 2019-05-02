53f6f3e993645db756e34cf0bc99827d41f6ab9f
Alexandru-Paul Copil <alexandru@cpl.li>
2019-05-02 22:42:28 +0300

    zync: fix import path

--- zync/doc.go
+++ zync/doc.go
@@ -6,4 +6,4 @@ packages are very good in general.
 Structures and functions will get removed from this package if Go gets similar
 or better implementations.
 */
-package zync
+package zync // import "cpl.li/go/cryptor/zync"
