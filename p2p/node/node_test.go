7c78f3325aae17eaab26f87161866ad358f27fee
Alexandru-Paul Copil <alexandru@cpl.li>
2019-05-02 13:51:34 +0300

    p2p/node: move state to separate file (including tests)

--- p2p/node/node_test.go
+++ p2p/node/node_test.go
@@ -134,31 +134,3 @@ func TestConnectInvalidAddress(t *testing.T) {
 	tests.AssertNotNil(t, n.Connect(), "connect on invalid address")
 	tests.AssertNil(t, n.Stop())
 }
-
-func TestNodeState(t *testing.T) {
-	n := node.NewNode("test", zeroKey)
-	tests.AssertEqual(t, n.State(), node.StateStopped,
-		"unexpected node state")
-	tests.AssertNil(t, n.Start())
-	tests.AssertEqual(t, n.State(), node.StateRunning,
-		"unexpected node state")
-	tests.AssertNil(t, n.Stop())
-	tests.AssertEqual(t, n.State(), node.StateStopped,
-		"unexpected node state")
-	tests.AssertNil(t, n.Start())
-	tests.AssertNil(t, n.Connect())
-	tests.AssertEqual(t, n.State(), node.StateConnected,
-		"unexpected node state")
-	tests.AssertNil(t, n.Stop())
-	tests.AssertEqual(t, n.State(), node.StateStopped,
-		"unexpected node state")
-}
-
-func TestNodeStateString(t *testing.T) {
-	tests.AssertEqual(t, node.StateStopped.String(), "STOPPED",
-		"unexpected state string")
-	tests.AssertEqual(t, node.StateRunning.String(), "RUNNING",
-		"unexpected state string")
-	tests.AssertEqual(t, node.StateConnected.String(), "CONNECTED",
-		"unexpected state string")
-}
