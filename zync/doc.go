2339f74c622f50685b885d133395f8177886de2f
Alexandru-Paul Copil <alexandru@cpl.li>
2019-05-02 20:51:01 +0300

    zync: add new package for custom sync solutions

---
+++ zync/doc.go
@@ -0,0 +1,9 @@
+/*
+Package zync contains custom simplified sync structures. It's sync with a Z.
+Might release this as a standalone package sometime. Doubt it as x/sync and sync
+packages are very good in general.
+
+Structures and functions will get removed from this package if Go gets similar
+or better implementations.
+*/
+package zync
